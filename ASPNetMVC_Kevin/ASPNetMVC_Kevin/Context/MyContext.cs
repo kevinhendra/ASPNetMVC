540a2274f084f84dc2c859d3b8bc9c920e3777d5
Kevin Hendra William <kevinhendrawiliam@gmail.com>
2020-10-22 15:12:16 +0700

    Add MyContext and migration database

---
+++ ASPNetMVC_Kevin/ASPNetMVC_Kevin/Context/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using ASPNetMVC_Kevin.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASPNetMVC_Kevin.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("ASPNetMVC_Kevin") { }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
