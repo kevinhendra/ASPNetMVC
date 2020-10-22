c9abbc9f99e2d7a06e58e4d227fbbd0d209a5b95
Kevin Hendra William <kevinhendrawiliam@gmail.com>
2020-10-22 16:18:08 +0700

    Create done

--- ASPNetMVC_Kevin/ASPNetMVC_Kevin/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNetMVC_Kevin.MvcApplication" Language="C#" %>
---
+++ ASPNetMVC_Kevin/Controllers/DepartmentsController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNetMVC_Kevin.Context;
+using ASPNetMVC_Kevin.Models;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetMVC_Kevin.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        MyContext myContext = new MyContext();
+        // GET: Departments
+        public ActionResult Index()
+        {
+            return View(myContext.Departments.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(myContext.Departments.Find(id));
+        }
+        //create viewnya
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //create aactionnya
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            myContext.Departments.Add(department);
+            myContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
